fa0c94466d6395cc6abe0c55e2af64ec071ae093
Davids-Adamovics <davidsadamovicss@gmail.com>
2023-12-09 14:11:04 +0200

    fix start menu

--- src/main/java/lv/venta/startmenu.java
+++ src/main/java/lv/venta/startmenu.java
@@ -1,29 +1,53 @@
 package lv.venta;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class startmenu extends Application {
 
     public static void main(String[] args) {
-
         launch(args);
-
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Image iconImage = new Image(getClass().getResource("logologo.png").toExternalForm());
-    	Scene myScene = FXMLLoader.load(getClass().getResource("startScreen.fxml"));
-        primaryStage.setTitle("IcySnake");
-        primaryStage.setScene(myScene);
-        primaryStage.getIcons().add(iconImage);
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        Image iconImage = loadImage("logologo.png");
+        if (iconImage == null) {
+            System.err.println("Error loading icon image.");
+        }
 
+        try {
+            Scene myScene = FXMLLoader.load(getClass().getResource("startScreen.fxml"));
+            primaryStage.setTitle("IcySnake");
+            primaryStage.setScene(myScene);
+            if (iconImage != null) {
+                primaryStage.getIcons().add(iconImage);
+            }
+            primaryStage.show();
+        } catch (IOException e) {
+            // Handle FXML loading exception
+            e.printStackTrace();
+        }
     }
 
+    private Image loadImage(String resourceName) {
+        try (InputStream stream = getClass().getResourceAsStream(resourceName)) {
+            if (stream != null) {
+                return new Image(stream);
+            } else {
+                System.err.println("Image resource not found: " + resourceName);
+            }
+        } catch (IOException e) {
+            // Handle IO exception
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
