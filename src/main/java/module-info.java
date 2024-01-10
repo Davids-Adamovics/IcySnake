ea7b28c767ee0ab75c305fbca258e4c6db325ca1
GrecAlek <s23grecalek@venta.lv>
2024-01-10 11:20:58 +0200

    option button
    uzlik option button bet neuzlik lai strada

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module lv.venta {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+	requires javafx.graphics;
 
     opens lv.venta to javafx.fxml;
     exports lv.venta;
